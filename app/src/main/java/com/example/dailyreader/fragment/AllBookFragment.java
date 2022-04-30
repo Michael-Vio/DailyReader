7cf5f1ac1559ac11e08b98e1e22d8f22e6e8a32d
Michael-Vio <70895002+Michael-Vio@users.noreply.github.com>
2022-04-30 14:41:04 +0800

    move the weather api by using the retrofit into home page - zhuang

--- app/src/main/java/com/example/dailyreader/fragment/AllBookFragment.java
+++ app/src/main/java/com/example/dailyreader/fragment/AllBookFragment.java
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dailyreader.WeatherApi;
 import com.example.dailyreader.adapter.AllBookFragmentAdapter;
 import com.example.dailyreader.databinding.AllBookFragmentBinding;
 import com.example.dailyreader.entity.Book;
@@ -23,6 +24,12 @@ import com.example.dailyreader.viewmodel.BookViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class AllBookFragment extends Fragment {
@@ -57,6 +64,43 @@ public class AllBookFragment extends Fragment {
         binding.allBookRecyclerView.addItemDecoration(new DividerItemDecoration(requireContext(),LinearLayoutManager.VERTICAL));
 
         binding.allBookRecyclerView.setHasFixedSize(true);
+
+
+        //Use weather API
+        //https://api.openweathermap.org/data/2.5/weather?lat=37.8136&lon=144.9631&appid=2e1018bb418dd391d11b25c5a7fad3a0
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/data/2.5/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        WeatherApi weatherApi = retrofit.create(WeatherApi.class);
+
+        Call<WeatherRoot> call = weatherApi.getWeather();
+
+        //Call
+        call.enqueue(new Callback<WeatherRoot>() {
+            @Override
+            public void onResponse(Call<WeatherRoot> call, Response<WeatherRoot> response){
+                WeatherRoot weatherRoot = response.body();
+
+                //temp change
+                double temp = weatherRoot.getMain().getTemp() - 273.15;
+                int pressure = weatherRoot.getMain().getPressure();
+                int humidity = weatherRoot.getMain().getHumidity();
+
+                //show temp
+                binding.weatherTemp.setText(String.valueOf((int)temp));
+                binding.pressure.setText(String.valueOf(pressure));
+                binding.humidity.setText(String.valueOf(humidity));
+            }
+
+            @Override
+            public void onFailure(Call<WeatherRoot> call, Throwable throwable){
+                System.out.println(throwable.getMessage());
+            }
+
+
+        });
 
         return view;
     }
