f0f908f7a13d9befa5b403af3930bf447fbc85d4
hzho83 <hzho83@student.monash.edu>
2022-05-04 02:46:52 +0800

    add map function(still need to fix, there is some bugs ) -- zhou

--- app/src/main/java/com/example/dailyreader/fragment/MapFragment.java
+++ app/src/main/java/com/example/dailyreader/fragment/MapFragment.java
@@ -8,18 +8,33 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.dailyreader.databinding.MapFragmentBinding;
+import com.mapbox.geojson.Point;
+import com.mapbox.maps.CameraOptions;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
 
 
 public class MapFragment extends Fragment {
     private MapFragmentBinding mapBinding;
 
-    public MapFragment () {}
+    private MapView mapView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final Point point = Point.fromLngLat(145.045837, -37.876823 );
         mapBinding = MapFragmentBinding.inflate(inflater, container, false);
         View view = mapBinding.getRoot();
+
+        CameraOptions cameraPosition = new CameraOptions.Builder()
+                .zoom(13.0)
+                .center(point)
+                .build();
+
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+        mapView.getMapboxMap().setCamera(cameraPosition);
+
         return view;
     }
     @Override
