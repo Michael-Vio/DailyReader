96665d9e34c598278c0651782e45e6b68f56b24d
YurongGao <i17012419@student.newinti.edu.my>
2022-05-04 21:22:34 +0800

    Record read time changed --gao

--- app/src/main/java/com/example/dailyreader/DAO/ReadTimeDAO.java
+++ app/src/main/java/com/example/dailyreader/DAO/ReadTimeDAO.java
@@ -1,70 +1,31 @@
 package com.example.dailyreader.DAO;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
 
-import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 
 import com.example.dailyreader.entity.ReadTime;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
+import java.util.List;
 
+@Dao
+public interface ReadTimeDAO {
 
-public class ReadTimeDAO {
-    private DatabaseReference databaseReference;
-
-    public ReadTimeDAO(String date)
-    {
-        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReference("users").child("records").child(date);
-    }
-
-    public Task<Void> add(ReadTime readTime) {
-
-        return databaseReference.setValue(readTime);
-    }
-
-    public Task<Void> update(String key, HashMap<String, Object> hashMap) {
-        return  databaseReference.child(key).updateChildren(hashMap);
-    }
-
-
-
-    public void upload(String date, int minutes) {
-
-        databaseReference.orderByChild("readDate").equalTo(date).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (snapshot.exists()) {
-                    HashMap<String, Object> readTime = new HashMap<>();
-
-                    readTime.put("readDate", date);
-                    readTime.put("readTime", minutes);
-                    update(snapshot.getKey(), readTime);
-                } else {
-                    ReadTime readTime = new ReadTime(date, minutes);
-                    add(readTime);
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
-    public Query get() {
-        return databaseReference.getParent().orderByChild("readDate");
-    }
+    @Query("SELECT * FROM readTime ORDER BY read_date ASC")
+    LiveData<List<ReadTime>> getAll();
+    @Query("SELECT * FROM readTime WHERE read_date = :readDate LIMIT 1")
+    ReadTime findByDate(String readDate);
+    @Insert
+    void insert(ReadTime readTime);
+    @Delete
+    void delete(ReadTime readTime);
+    @Update
+    void updateReadTime(ReadTime readTime);
+    @Query("DELETE FROM readTime")
+    void deleteAll();
 
 }
-
