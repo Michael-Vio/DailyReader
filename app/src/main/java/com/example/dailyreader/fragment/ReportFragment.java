1d51c43f61ff4e4d4280b7825daa9c9328a48aea
Michael-Vio <70895002+Michael-Vio@users.noreply.github.com>
2022-05-05 20:52:54 +0800

    try to switch the chart- zhuang

--- app/src/main/java/com/example/dailyreader/fragment/ReportFragment.java
+++ app/src/main/java/com/example/dailyreader/fragment/ReportFragment.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.anychart.APIlib;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
 import com.example.dailyreader.R;
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
@@ -51,6 +60,7 @@ public class ReportFragment extends Fragment {
 
         AnyChartView anyChartView = view.findViewById(R.id.any_chart_view);
         anyChartView.setProgressBar(view.findViewById(R.id.progress_bar));
+        APIlib.getInstance().setActiveAnyChartView(anyChartView);
 
         Pie pie = AnyChart.pie();
 
@@ -61,22 +71,28 @@ public class ReportFragment extends Fragment {
             }
         });*/
 
+        //get the data from the list
+        /*for (LiveData<List<ReadTime>> readTime : readTimeList) {
+
+        }*/
+
         List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("Apples", 6371664));
-        data.add(new ValueDataEntry("Pears", 789622));
-        data.add(new ValueDataEntry("Bananas", 7216301));
-        data.add(new ValueDataEntry("Grapes", 1486621));
-        data.add(new ValueDataEntry("Oranges", 1200000));
+        data.add(new ValueDataEntry("2022-05-01", 35));
+        data.add(new ValueDataEntry("2022-05-02", 23));
+        data.add(new ValueDataEntry("2022-05-03", 11));
+        data.add(new ValueDataEntry("2022-05-04", 6));
+        data.add(new ValueDataEntry("2022-05-05", 66));
+        data.add(new ValueDataEntry("2022-05-06", 76));
 
         pie.data(data);
 
-        pie.title("Fruits imported in 2015 (in kg)");
+        pie.title("Your daily reading time");
 
         pie.labels().position("outside");
 
         pie.legend().title().enabled(true);
         pie.legend().title()
-                .text("Retail channels")
+                .text("Minute")
                 .padding(0d, 0d, 10d, 0d);
 
         pie.legend()
@@ -86,10 +102,85 @@ public class ReportFragment extends Fragment {
 
         anyChartView.setChart(pie);
 
+        //create column chart
+        AnyChartView anyChartView1 = view.findViewById(R.id.any_chart_view);
+        APIlib.getInstance().setActiveAnyChartView(anyChartView1);
+
+        Cartesian cartesian = AnyChart.column();
+
+
+        List<DataEntry> data1 = new ArrayList<>();
+        data1.add(new ValueDataEntry("2022-05-01", 35));
+        data1.add(new ValueDataEntry("2022-05-02", 23));
+        data1.add(new ValueDataEntry("2022-05-03", 11));
+        data1.add(new ValueDataEntry("2022-05-04", 6));
+        data1.add(new ValueDataEntry("2022-05-05", 66));
+        data1.add(new ValueDataEntry("2022-05-06", 76));
+
+        Column column = cartesian.column(data1);
+
+        column.tooltip()
+                .titleFormat("{%X}")
+                .position(Position.CENTER_BOTTOM)
+                .anchor(Anchor.CENTER_BOTTOM)
+                .offsetX(0d)
+                .offsetY(5d)
+                .format("{%Value}{groupsSeparator: }");
+
+        cartesian.animation(true);
+        cartesian.title("Your daily reading time");
+
+        cartesian.yScale().minimum(0d);
+
+        cartesian.yAxis(0).labels().format("{%Value}{groupsSeparator: }");
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+        cartesian.interactivity().hoverMode(HoverMode.BY_X);
+
+        cartesian.xAxis(0).title("Date");
+        cartesian.yAxis(0).title("Time (Minute)");
+
+
+        Switch mSwitch = (Switch) view.findViewById(R.id.switch1);
+
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // The toggle is enabled
+                    anyChartView.setChart(pie);
+
+                } else {
+                    // The toggle is disabled
+                    anyChartView1.setChart(cartesian);
+
+
+                }
+            }
+        });
+
+        /*f(mSwitch.isChecked()){
+            anyChartView.setChart(pie);;
+        }
+        else{
+            anyChartView.setChart(cartesian);
+        }*/
+
+        //Generate pie chart
+        //anyChartView.setChart(pie);
+
+        //Generate column chart
+        //anyChartView.setChart(cartesian);
+
+        /*APIlib.getInstance().setActiveAnyChartView(anyChartView);
+        pie.title("First chart");
+        APIlib.getInstance().setActiveAnyChartView(anyChartView1);
+        cartesian.title("Second chart");*/
 
 
         return view;
     }
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
