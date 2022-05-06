f116a940a0d7ae5f7218134e9c54d722367d0ab0
IvanHuang <thua0039@student.monash.edu>
2022-05-06 23:29:30 +1000

    Login activity validation check update -- Tao Huang 32642172

--- app/src/main/java/com/example/dailyreader/MainActivity.java
+++ app/src/main/java/com/example/dailyreader/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.dailyreader;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Toast;
 
@@ -31,24 +32,50 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, RegisterActivity.class));
         });
 
-        // validation
-
-        binding.loginButton.setOnClickListener(v ->{
-            mAuth.signInWithEmailAndPassword(binding.emailInput.getText().toString(), binding.passwordInput.getText().toString())
-                    .addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(this, "Login successfully!",
-                                    Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(this, HomeActivity.class));
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(this, "Login failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
+        binding.loginButton.setOnClickListener(v -> {
+            if (loginCheck()) {
+                mAuth.signInWithEmailAndPassword(binding.emailInput.getText().toString(), binding.passwordInput.getText().toString())
+                        .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(this, "Login successfully!",
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(this, HomeActivity.class));
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(this, "                       Login failed." +
+                                                "\nPlease check your Email and password!",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
         });
     }
-}
 
+    private boolean loginCheck() {
+        Boolean validEmail = false;
+        Boolean validPassword = false;
+        Editable emailInput = binding.emailInput.getText();
+        Editable passwordInput = binding.passwordInput.getText();
+        if (emailInput.toString().isEmpty()) {
+            binding.emailError.setError(getResources().getString(R.string.email_error));
+            validEmail = false;
+        } else {
+            binding.emailError.setErrorEnabled(false);
+            validEmail = true;
+        }
+        if (passwordInput.toString().isEmpty()) {
+            binding.passwordError.setError(getResources().getString(R.string.password_empty_error));
+            validPassword = false;
+        } else {
+            binding.passwordError.setErrorEnabled(false);
+            validPassword = true;
+        }
+        if (validEmail && validPassword) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
