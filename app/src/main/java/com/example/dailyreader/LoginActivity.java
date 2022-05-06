2dc6f49773555573e54e5fe2ce4134d9ac1c7766
IvanHuang <thua0039@student.monash.edu>
2022-05-06 17:55:57 +1000

    Login activity authentication update -- Tao Huang 32642172

--- app/src/main/java/com/example/dailyreader/LoginActivity.java
+++ app/src/main/java/com/example/dailyreader/LoginActivity.java
@@ -3,15 +3,19 @@ package com.example.dailyreader;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.dailyreader.databinding.LoginActivityBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginActivityBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,29 @@ public class LoginActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        mAuth = FirebaseAuth.getInstance();
+
         binding.signUp.setOnClickListener(v -> {
             startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+        });
+
+        // validation
+
+        binding.loginButton.setOnClickListener(v ->{
+            mAuth.signInWithEmailAndPassword(binding.emailInput.getText().toString(), binding.passwordInput.getText().toString())
+                    .addOnCompleteListener(this, task -> {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(LoginActivity.this, "Login successfully!",
+                                        Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Toast.makeText(LoginActivity.this, "Login failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                    });
         });
     }
 }
